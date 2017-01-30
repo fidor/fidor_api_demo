5633ea2f66f36cb4eca405c5f67ba66fc9283046
Klaus Meyer <spam@klaus-meyer.net>
2017-01-30 20:02:09 +0100

    hide setup page if setup is complete

--- app/controllers/setup/configs_controller.rb
+++ app/controllers/setup/configs_controller.rb
@@ -1,6 +1,14 @@
 class Setup::ConfigsController < ApplicationController
   skip_before_action :require_complete_setup
 
+  before_action :require_incomplete_setup
+
   def index
+  end
+
+  private
+
+  def require_incomplete_setup
+    redirect_to root_path unless EnvConfig.incomplete?
   end
 end
